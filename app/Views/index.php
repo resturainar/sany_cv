eb929b6fc90033633ef1462ea8c2ae4d85eef6e0
resturainar <resturayinarahmah442@gmail.com>
2023-07-17 01:07:44 +0700

    CV

---
+++ app/Views/index.php
@@ -0,0 +1,408 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SANY</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <!-- Place favicon.ico in the root directory -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="/melan/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/melan/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/melan/css/magnific-popup.css">
+    <link rel="stylesheet" href="/melan/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/melan/css/themify-icons.css">
+    <link rel="stylesheet" href="/melan/css/nice-select.css">
+    <link rel="stylesheet" href="/melan/css/flaticon.css">
+    <link rel="stylesheet" href="/melan/css/gijgo.css">
+    <link rel="stylesheet" href="/melan/css/animate.min.css">
+    <link rel="stylesheet" href="/melan/css/slick.css">
+    <link rel="stylesheet" href="/melan/css/slicknav.css">
+    <link rel="stylesheet" href="/melan/css/style.css">
+    <!-- <link rel="stylesheet" href="/melan/css/responsive.css"> -->
+</head>
+
+<body>
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo"></div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="main-menu  d-none d-lg-block topbar">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a class="active" href="index.html">home</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="services.html">Services</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                            <div class="Appointment">
+                                <div class="book_btn d-none d-lg-block">
+                                    <a href="#">Contact Me</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="single_slider  d-flex align-items-center slider_bg_1">
+            <div class="social_links">
+                <ul>
+                    <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+                    <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+                    <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-12">
+                        <div class="slider_text text-center">
+                            <h3>Sany Santiastry's here</h3>
+                            <span>i'm a web developer</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider_area_end -->
+
+    <!-- service_area  -->
+    <div class="service_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center mb-65">
+                        <h3>Services I Provide</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-file-code icons"></i>
+                        <h3>Web Development</h3>
+                        <p>creating and maintaining websites and web applications using various programming languages and frameworks.</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-pen-ruler icons"></i>
+                        <h3>UI/UX Design</h3>
+                        <p>creating intuitive and visually appealing interfaces that enhance user experience and satisfaction when interacting with digital products.</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-mobile icons"></i>
+                        <h3>Mobile App</h3>
+                        <p>creating applications specifically designed to run on mobile devices such as smartphones and tablets, using platforms like iOS or Android.</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-file-contract icons"></i>
+                        <h3>Branding</h3>
+                        <p>strategic process of developing a unique identity to differentiate and create a lasting impression on customers.</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-chart-simple icons"></i>
+                        <h3>Marketing</h3>
+                        <p>promoting products or services through various channels to reach and engage with target audiences.</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_service text-center">
+                    <i class="fa-solid fa-comments icons"></i>
+                        <h3>IT Consultation</h3>
+                        <p>advice and guidance on technology-related issues, helping businesses make informed decisions regarding their overall digital strategy.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ service_area  -->
+
+    <section class="page-section single-slider slider_bg_1" id="skill">
+        <div class="container">
+            <div class="skill_title text-center mb-65">
+                        <h3>My Skills</h3>
+                    </div>
+            <hr class="divider my-4">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="far fa-file-code fa-fw text-warning small-icon"></i>
+                        <h3 class="h4 mb-2 text-white">HTML5/CSS3</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-js-square fa-fw text-warning small-icon"></i>
+                        <h3 class="h4 mb-2 text-white">JavaScript</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-react fa-fw text-warning small-icon"></i>
+                        <h3 class="h4 mb-2 text-white">ReactJs</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-bootstrap fa-fw text-warning small-icon"></i>
+                        <h3 class="h4 mb-2 text-white">Bootstrap</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- portfolio_area -->
+    <div class="portfolio_area portfolio_bg_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title white_text text-center">
+                        <span>Portfolios</span>
+                        <h3>Some of My Works Here</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ portfolio_area -->
+
+    <!-- portfolio_image_area  -->
+    <div class="portfolio_image_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-md-5">
+                    <div class="single_Portfolio">
+                        <div class="portfolio_thumb">
+                            <img src="/melan/img/portfolio/1.png" alt="">
+                        </div>
+                        <a href="img/portfolio/1.png" class="popup popup-image"></a>
+                        <div class="portfolio_hover">
+                            <div class="title">
+                                <h3>Product Design</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-md-7">
+                    <div class="single_Portfolio">
+                        <div class="portfolio_thumb">
+                            <img src="/melan/img/portfolio/2.png" alt="">
+                        </div>
+                        <a href="img/portfolio/2.png" class="popup popup-image"></a>
+                        <div class="portfolio_hover">
+                            <div class="title">
+                                <h3>Product Design</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-md-6 col-lg-4">
+                    <div class="single_Portfolio">
+                        <div class="portfolio_thumb">
+                            <img src="/melan/img/portfolio/3.png" alt="">
+                        </div>
+                        <a href="img/portfolio/3.png" class="popup popup-image"></a>
+                        <div class="portfolio_hover">
+                            <div class="title">
+                                <h3>Product Design</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-md-6 col-lg-4">
+                    <div class="single_Portfolio">
+                        <div class="portfolio_thumb">
+                            <img src="/melan/img/portfolio/4.png" alt="">
+                        </div>
+                        <a href="img/portfolio/4.png" class="popup popup-image"></a>
+                        <div class="portfolio_hover">
+                            <div class="title">
+                                <h3>Product Design</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="more_portfolio text-center">
+                        <a class="line_btn" href="#">More Folio</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ portfolio_image_area  -->
+
+    <!-- about_me  -->
+    <div class="about_me">
+        <div class="about_large_title d-none d-lg-block">
+            About
+        </div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-md-6">
+                    <div class="about_e_details">
+                        <h3>About me</h3>
+                        <p>Hi! My name is Sany and I am currently studying IT. Always eager to learn and grow, I seek opportunities to expand my knowledge and skills. With a focus on staying updated in the ever-evolving IT industry, I am committed to continuous learning. I aim to make valuable contributions in domains like software development and emerging technologies. I am excited to connect with professionals and explore potential collaborations in the IT community. Let's embark on a journey of growth and success together!</p>
+                        <div class="download_cv">
+                            <a class="boxed-btn3" href="#">Download CV</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="about_img">
+                        <div class="color_pattern d-none d-lg-block">
+                            <img src="/melan/img/about/color_grid.png" alt="">
+                        </div>
+                        <div class="my_Pic">
+                            <img src="/melan/img/about/sany.jpeg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ about_me  -->
+
+    <div class="counter_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_counter text-center">
+                        <h3>
+                            <span class="counter">147 </span><span>+</span>
+                        </h3>
+                        <p>Total Projects</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_counter text-center">
+                        <h3>
+                            <span class="counter">18 </span>
+                        </h3>
+                        <p>On Going Projects</p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single_counter text-center">
+                        <h3>
+                            <span class="counter">95 </span> <span>%</span>
+                        </h3>
+                        <p>Job Success</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="discuss_projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="project_text text-center">
+                        <h3>Let’s discuss for a project</h3>
+                        <p>Explore and analyze the details, requirements, and goals of a specific project, with the intention of gaining a deeper understanding and determining the most effective strategies and approaches to ensure a successful execution and accomplishment.</p>
+                        <a class="boxed-btn3" href="#">Start Talking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="menu_links">
+                            <ul>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="socail_links">
+                            <ul>
+                                <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer end  -->
+
+    <!-- JS here -->
+    <script src="/melan/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="/melan/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="/melan/js/popper.min.js"></script>
+    <script src="/melan/js/bootstrap.min.js"></script>
+    <script src="/melan/js/owl.carousel.min.js"></script>
+    <script src="/melan/js/isotope.pkgd.min.js"></script>
+    <script src="/melan/js/ajax-form.js"></script>
+    <script src="/melan/js/waypoints.min.js"></script>
+    <script src="/melan/js/jquery.counterup.min.js"></script>
+    <script src="/melan/js/imagesloaded.pkgd.min.js"></script>
+    <script src="/melan/js/scrollIt.js"></script>
+    <script src="/melan/js/jquery.scrollUp.min.js"></script>
+    <script src="/melan/js/wow.min.js"></script>
+    <script src="/melan/js/nice-select.min.js"></script>
+    <script src="/melan/js/jquery.slicknav.min.js"></script>
+    <script src="/melan/js/jquery.magnific-popup.min.js"></script>
+    <script src="/melan/js/plugins.js"></script>
+    <script src="/melan/js/gijgo.min.js"></script>
+
+    <!--contact js-->
+    <script src="/melan/js/contact.js"></script>
+    <script src="/melan/js/jquery.ajaxchimp.min.js"></script>
+    <script src="/melan/js/jquery.form.js"></script>
+    <script src="/melan/js/jquery.validate.min.js"></script>
+    <script src="/melan/js/mail-script.js"></script>
+
+    <script src="/melan/js/main.js"></script>
+</body>
+
+</html>
